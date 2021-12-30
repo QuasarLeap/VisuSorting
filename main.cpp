db5c37e53b9982e97f9f29cbb5bae90a257a2ed5
QuasarLeap <MaverickDin@users.noreply.github.com>
2021-12-30 14:19:17 -0500

    Added selection sort

--- main.cpp
+++ main.cpp
@@ -24,17 +24,17 @@ void printArray(vector<int> &numArr){
     }
 }
 
-void swapPos(vector<int> &numArr, int first, int sec){
-    int temp = numArr[first];
-    numArr[first] = numArr[sec];
-    numArr[sec] = temp;
+void swapPos(vector<int> &numArr, int firstPos, int secPos){
+    int temp = numArr[firstPos];
+    numArr[firstPos] = numArr[secPos];
+    numArr[secPos] = temp;//swap with the stored first number
 }
 
 void bubbleSort(vector<int> &numArr){
     bool swapped;
     for(int i = 0; numArr.size()-1; i++){
         swapped = false;
-        for(int j = 0; j < numArr.size()-i-1; j++){
+        for(int j = 0; j < numArr.size()-i-1; j++){//bring largest number to the end of vector/array
             if(numArr[j] > numArr[j+1]){
                 swapPos(numArr, j, j+1);
                 swapped = true;
@@ -46,11 +46,29 @@ void bubbleSort(vector<int> &numArr){
     }
 }
 
-void selectionSort(){
+void selectionSort(vector<int> &numArr){
+    //current minimum index/item variable
+    //set current minimum to the first unsorted element
+
+    for(int i = 0; i < numArr.size(); i++){
+        int curMinI = i;
+        //look for smaller number
+        for(int j = i+1; j < numArr.size(); j++){
+            if(numArr[j] < numArr[curMinI]){
+                curMinI = j;
+            }
+        }
+        //swap numbers
+        //go to next unsorted position        
+        swapPos(numArr, i, curMinI);
+    }
+    
+
 
 }
 
-void insertionSort(){
+void insertionSort(vector<int> &numArr){
+    
 
 }
 
@@ -69,15 +87,16 @@ void heapSort(){
 
 int main(int argc, char *argv[]){
 
-    const int A_SIZE = 10000;
+    const int A_SIZE = 10;
     vector<int> numArr;
 
     insertRandNum(numArr, A_SIZE);
 
     printArray(numArr);
-    bubbleSort(numArr);
-    
-    cout << "----BUBBLE SORTED----" << endl;
+    //bubbleSort(numArr);
+    selectionSort(numArr);
+    //cout << "----BUBBLE SORTED----" << endl;
+    cout << "-----SELECTION SORTED-----" << endl;
     printArray(numArr);
 
     return 0;
